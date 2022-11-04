993d505ee2c20b9e56e252071e0b951bb8f6f618
An Nguyen <nguyen.an.1196@gmail.com>
2022-11-04 15:58:13 -0700

    Make Firebase save user info when they log in with their username

--- app/src/main/java/edu/northeastern/nowornever/StickerMessagingActivity.java
+++ app/src/main/java/edu/northeastern/nowornever/StickerMessagingActivity.java
@@ -2,9 +2,11 @@ package edu.northeastern.nowornever;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,8 +15,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 
+import java.util.ArrayList;
+
+import edu.northeastern.nowornever.model.sticker.Sticker;
 import edu.northeastern.nowornever.model.sticker.User;
 
 public class StickerMessagingActivity extends AppCompatActivity {
@@ -25,7 +33,6 @@ public class StickerMessagingActivity extends AppCompatActivity {
     private String username;
     private EditText receiverUsername;
     private RadioButton sticker;
-    // private Button sendStickerBtn, aboutBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,12 +53,13 @@ public class StickerMessagingActivity extends AppCompatActivity {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 showMostRecent(snapshot);
-                Log.e(TAG, "onChildAdded: dataSnapshot = " + snapshot.getValue().toString());
+                Log.e(TAG, "onChildAdded: snapshot = " + snapshot.getValue().toString());
             }
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-
+                showMostRecent(snapshot);
+                Log.e(TAG, "onChildChanged: snapshot = " + snapshot.getValue().toString());
             }
 
             @Override
@@ -66,16 +74,82 @@ public class StickerMessagingActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Log.e(TAG, "onCancelled:" + error);
+                Toast.makeText(getApplicationContext()
+                        , "DBError: " + error, Toast.LENGTH_SHORT).show();
             }
+        });
+
+        // TODO: extra logics for existing user and data -> refactor this for received stickers
+        User user = new User(this.username, new ArrayList<>());
+        database.getReference().child("users").child(user.username).setValue(user);
+
+    }
+
+    public void sendSticker(View view) {
+        StickerMessagingActivity.this.onSend(database.getReference());
+    }
+
+    private void onSend(DatabaseReference reference) {
+        String receiver = receiverUsername.getText().toString();
+        if (receiver.matches("") || receiver.equals(this.username)) {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // TODO: check if user exist in database
+        // if (reference.child("user").child(receiver).get)
+
+        reference.child("users").child(receiver).runTransaction(new Transaction.Handler() {
+            @NonNull
+            @Override
+            public Transaction.Result doTransaction(@NonNull MutableData mutableData) {
+
+                User user = mutableData.getValue(User.class);
+
+                if (user == null) {
+                    return Transaction.success(mutableData);
+                }
+
+                Sticker sticker = new Sticker(checkStickerType(), StickerMessagingActivity.this.username);
+                user.receivedStickers.add(sticker);
+
+                mutableData.setValue(user);
+
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(DatabaseError databaseError, boolean successful,
+                                   DataSnapshot dataSnapshot) {
+                // Transaction completed
+                Log.d(TAG, "postTransaction:onComplete:" + databaseError);
+                if (successful) {
+                    Toast.makeText(StickerMessagingActivity.this,
+                            "Sticker Sent", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(StickerMessagingActivity.this
+                            , "DBError: " + databaseError, Toast.LENGTH_SHORT).show();
+                }
+            }
+
         });
     }
 
+    private String checkStickerType() {
+        if (sticker.isChecked()) {
+            return "Type 1";
+        }
+        return "Type 2";
+    }
+
+
     private void showMostRecent(DataSnapshot snapshot) {
         User user = snapshot.getValue(User.class);
-        if (snapshot.getKey() != null) {
-            recentReceivedStickerView.setText(String.valueOf(user.receivedStickers.get(-1)));
-        }
+        return;
+        // TODO: this logic is wrong, refactor as mentioned as above TODO
+//        if (snapshot.getKey() != null && !user.receivedStickers.isEmpty()) {
+//            recentReceivedStickerView.setText(String.valueOf(user.receivedStickers.get(-1)));
+//        }
     }
 
 
