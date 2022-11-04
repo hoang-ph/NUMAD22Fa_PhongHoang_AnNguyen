1d28d8b3b6164f7a79b696b6ccf12d30a9f1913a
An Nguyen <nguyen.an.1196@gmail.com>
2022-11-04 12:03:26 -0700

    Add Firebase database connection

--- app/src/main/java/edu/northeastern/nowornever/StickerMessagingActivity.java
+++ app/src/main/java/edu/northeastern/nowornever/StickerMessagingActivity.java
@@ -1,14 +1,31 @@
 package edu.northeastern.nowornever;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class StickerMessagingActivity extends AppCompatActivity {
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
 
-    private TextView usernameView;
+import edu.northeastern.nowornever.model.sticker.User;
+
+public class StickerMessagingActivity extends AppCompatActivity {
+    private static final String TAG = StickerMessagingActivity.class.getSimpleName();
+
+    private FirebaseDatabase database;
+    private TextView usernameView, recentReceivedStickerView;
+    private String username;
+    private EditText receiverUsername;
+    private RadioButton sticker;
+    // private Button sendStickerBtn, aboutBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,7 +33,50 @@ public class StickerMessagingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sticker_messaging);
 
         usernameView = findViewById(R.id.usernameView);
-        String username = getIntent().getStringExtra("usernameKey");
+        username = getIntent().getStringExtra("usernameKey");
         usernameView.setText(username);
+
+        recentReceivedStickerView = findViewById(R.id.recentReceivedSticker);
+        receiverUsername = findViewById(R.id.receiverUsername);
+        sticker = findViewById(R.id.sticker1);
+
+        database = FirebaseDatabase.getInstance();
+
+        database.getReference().child("users").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                showMostRecent(snapshot);
+                Log.e(TAG, "onChildAdded: dataSnapshot = " + snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
+    private void showMostRecent(DataSnapshot snapshot) {
+        User user = snapshot.getValue(User.class);
+        if (snapshot.getKey() != null) {
+            recentReceivedStickerView.setText(String.valueOf(user.receivedStickers.get(-1)));
+        }
+    }
+
+
 }
