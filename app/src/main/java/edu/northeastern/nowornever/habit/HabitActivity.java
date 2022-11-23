416cb2bdbb58161526e7b11bbc747cd5b9caa027
An Nguyen <nguyen.an.1196@gmail.com>
2022-11-22 17:16:18 -0800

    Bind fragments with functionality

--- app/src/main/java/edu/northeastern/nowornever/habit/HabitActivity.java
+++ app/src/main/java/edu/northeastern/nowornever/habit/HabitActivity.java
@@ -1,12 +1,17 @@
 package edu.northeastern.nowornever.habit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import edu.northeastern.nowornever.R;
+import edu.northeastern.nowornever.databinding.ActivityHabitBinding;
 
 public class HabitActivity extends AppCompatActivity {
 
@@ -15,11 +20,15 @@ public class HabitActivity extends AppCompatActivity {
     private String username;
     private SharedPreferences sharedPreferences;
     private TextView testHabit;
+    private ActivityHabitBinding binding;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_habit);
+        binding = ActivityHabitBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        displaySelectedFrag(new HomeFragment());
 
         testHabit = findViewById(R.id.testHabit);
 
@@ -27,8 +36,37 @@ public class HabitActivity extends AppCompatActivity {
 
         username = sharedPreferences.getString(USERNAME_KEY, null);
         if (username != null) {
-            testHabit.setText(username);
+            String title = "Username: " + username; // Replace/ stop using this later
+            testHabit.setText(title);
         }
+
+        binding.habitBotNavBar.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.homeFrag:
+                    displaySelectedFrag(new HomeFragment());
+                    break;
+                case R.id.noteFrag:
+                    displaySelectedFrag(new NoteFragment());
+                    break;
+                case R.id.statFrag:
+                    displaySelectedFrag(new StatFragment());
+                    break;
+                case R.id.mediaFrag:
+                    displaySelectedFrag(new MediaFragment());
+                    break;
+                case R.id.friendFrag:
+                    displaySelectedFrag(new FriendFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void displaySelectedFrag(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.habitFrameLayout, fragment);
+        fragmentTransaction.commit();
     }
 
 }
