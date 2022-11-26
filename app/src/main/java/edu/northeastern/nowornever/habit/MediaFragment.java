039cfd057c52bb3ec73c9bf69b96cb32ce04e187
An Nguyen <nguyen.an.1196@gmail.com>
2022-11-26 09:19:31 -0800

    Setup UI for Media Fragment

--- app/src/main/java/edu/northeastern/nowornever/habit/MediaFragment.java
+++ app/src/main/java/edu/northeastern/nowornever/habit/MediaFragment.java
@@ -1,5 +1,8 @@
 package edu.northeastern.nowornever.habit;
 
+import static edu.northeastern.nowornever.utils.Constants.HABIT_ID_KEY;
+import static edu.northeastern.nowornever.utils.Constants.USERNAME_KEY;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,57 +13,19 @@ import android.view.ViewGroup;
 
 import edu.northeastern.nowornever.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MediaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MediaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MediaFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MediaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MediaFragment newInstance(String param1, String param2) {
-        MediaFragment fragment = new MediaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private String habitUuid, habitName, username;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_media, container, false);
+        View view = inflater.inflate(R.layout.fragment_media, container, false);
+        habitUuid = getArguments().getString(HABIT_ID_KEY);
+        username = getArguments().getString(USERNAME_KEY);
+
+        return view;
     }
 }
