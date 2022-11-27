f68f36eeb691b2c70804c42f7f07b633bdf4302c
An Nguyen <nguyen.an.1196@gmail.com>
2022-11-26 20:28:57 -0800

    Enable preview for selected image

--- app/src/main/java/edu/northeastern/nowornever/habit/MediaFragment.java
+++ app/src/main/java/edu/northeastern/nowornever/habit/MediaFragment.java
@@ -6,8 +6,15 @@ import static edu.northeastern.nowornever.utils.Constants.HABIT_ID_KEY;
 import static edu.northeastern.nowornever.utils.Constants.ROOT_HABIT;
 import static edu.northeastern.nowornever.utils.Constants.USERNAME_KEY;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
@@ -15,6 +22,8 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
@@ -29,6 +38,7 @@ public class MediaFragment extends Fragment {
 
     private List<String> imgIDs;
     private RecyclerView imgRecyclerView;
+    private ImageView selectedImgView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,6 +48,7 @@ public class MediaFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_media, container, false);
         String habitUuid = getArguments().getString(HABIT_ID_KEY);
         String username = getArguments().getString(USERNAME_KEY);
+        selectedImgView = view.findViewById(R.id.selectedImg);
 
         imgIDs = new ArrayList<>();
         imgRecyclerView = view.findViewById(R.id.imageRecyclerView);
@@ -45,6 +56,10 @@ public class MediaFragment extends Fragment {
         HabitImgRecyclerViewAdapter adapter = new HabitImgRecyclerViewAdapter(getContext(), imgIDs, username);
         imgRecyclerView.setAdapter(adapter);
 
+        Button selectImgBtn = view.findViewById(R.id.selectImageBtn);
+        selectImgBtn.setOnClickListener(v -> selectImage());
+
+        // TODO: redo this functionality to load all images - reimplement upload image and firebase architecture
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(ROOT_HABIT).child(username).child(CHILD_HABIT).child(habitUuid);
         ref.get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
@@ -61,4 +76,26 @@ public class MediaFragment extends Fragment {
 
         return view;
     }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        selectedImgLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> selectedImgLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Uri imgUri = data.getData();
+                        selectedImgView.setImageURI(imgUri);
+                    }
+                }
+            });
+
+
 }
