302b2dc89d2c82b3f52f504ea978fa476e7a9f7d
An Nguyen <nguyen.an.1196@gmail.com>
2022-11-26 21:23:52 -0800

    Enable image uploading to firebase storage

--- app/src/main/java/edu/northeastern/nowornever/habit/MediaFragment.java
+++ app/src/main/java/edu/northeastern/nowornever/habit/MediaFragment.java
@@ -2,8 +2,12 @@ package edu.northeastern.nowornever.habit;
 
 import static edu.northeastern.nowornever.utils.Constants.CHILD_HABIT;
 import static edu.northeastern.nowornever.utils.Constants.CHILD_IMG_ID;
+import static edu.northeastern.nowornever.utils.Constants.FAIL_UPLOAD;
 import static edu.northeastern.nowornever.utils.Constants.HABIT_ID_KEY;
+import static edu.northeastern.nowornever.utils.Constants.LOADING;
 import static edu.northeastern.nowornever.utils.Constants.ROOT_HABIT;
+import static edu.northeastern.nowornever.utils.Constants.SUCCESS_UPLOAD;
+import static edu.northeastern.nowornever.utils.Constants.UPLOAD;
 import static edu.northeastern.nowornever.utils.Constants.USERNAME_KEY;
 
 import android.app.Activity;
@@ -24,11 +28,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,8 +45,12 @@ import edu.northeastern.nowornever.R;
 public class MediaFragment extends Fragment {
 
     private List<String> imgIDs;
+    private String username;
     private RecyclerView imgRecyclerView;
     private ImageView selectedImgView;
+    private Button uploadImageBtn;
+    private StorageReference storageReference;
+    private Uri imgUri;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,7 +59,7 @@ public class MediaFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_media, container, false);
         String habitUuid = getArguments().getString(HABIT_ID_KEY);
-        String username = getArguments().getString(USERNAME_KEY);
+        this.username = getArguments().getString(USERNAME_KEY);
         selectedImgView = view.findViewById(R.id.selectedImg);
 
         imgIDs = new ArrayList<>();
@@ -58,6 +70,9 @@ public class MediaFragment extends Fragment {
 
         Button selectImgBtn = view.findViewById(R.id.selectImageBtn);
         selectImgBtn.setOnClickListener(v -> selectImage());
+
+        uploadImageBtn = view.findViewById(R.id.uploadImageBtn);
+        uploadImageBtn.setOnClickListener(v -> uploadImage());
 
         // TODO: redo this functionality to load all images - reimplement upload image and firebase architecture
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(ROOT_HABIT).child(username).child(CHILD_HABIT).child(habitUuid);
@@ -91,11 +106,38 @@ public class MediaFragment extends Fragment {
                 public void onActivityResult(ActivityResult result) {
                     if (result.getResultCode() == Activity.RESULT_OK) {
                         Intent data = result.getData();
-                        Uri imgUri = data.getData();
+                        imgUri = data.getData();
                         selectedImgView.setImageURI(imgUri);
                     }
                 }
             });
 
+
+    private void uploadImage() {
+        if (imgUri == null) {
+            Toast.makeText(getContext(), "An image has to be selected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        uploadImageBtn.setText(LOADING);
+        String imgID = String.valueOf(Instant.now().toEpochMilli());
+
+        storageReference = FirebaseStorage.getInstance().getReference("images/" + this.username + "/" + imgID);
+        storageReference.putFile(imgUri)
+                .addOnSuccessListener(taskSnapshot -> {
+                    selectedImgView.setImageURI(null);
+                    imgUri = null;
+                    Toast.makeText(getContext(), SUCCESS_UPLOAD, Toast.LENGTH_SHORT).show();
+                    if (uploadImageBtn.getText().equals(LOADING)) {
+                        uploadImageBtn.setText(UPLOAD);
+                    }
+                }).addOnFailureListener(e -> {
+                    if (uploadImageBtn.getText().equals(LOADING)) {
+                        uploadImageBtn.setText(UPLOAD);
+                    }
+                    Toast.makeText(getContext(), FAIL_UPLOAD, Toast.LENGTH_SHORT).show();
+                });
+
+    }
 
 }
--- app/src/main/java/edu/northeastern/nowornever/utils/Constants.java
+++ app/src/main/java/edu/northeastern/nowornever/utils/Constants.java
@@ -1,5 +1,7 @@
 package edu.northeastern.nowornever.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import java.util.Map;
 
 import edu.northeastern.nowornever.R;
@@ -29,6 +31,15 @@ public class Constants {
             SERVER_ERROR = "Server error - fail to get existing data";
 
     public static final long ONE_DAY_IN_EPOCH = 86500000L;
+
+    // Media Fragment
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("EEE MMM dd yyyy", Locale.getDefault());
+
+    public static final String
+            UPLOAD = "Upload Image",
+            LOADING = "Loading.....",
+            SUCCESS_UPLOAD = "Successfully uploaded",
+            FAIL_UPLOAD = "Failed to upload image";
 
     // Reference here: https://developer.android.com/reference/android/R.drawable.html
     public static final Map<String, Integer> STICKER_RESOURCES = Map.of(
